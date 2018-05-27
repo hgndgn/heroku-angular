673999367ff4f65a723e2cf998376f44fe666821
H.Gndgn <hueseyin.guendogan@gmail.com>
2018-05-27 19:07:29 +0200

    read env var

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { environment } from './../environments/environment';
+import { Component, OnInit } from '@angular/core';
+import * as env from 'env-var';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Heroku-Deployment-App Last update today :)';
+  username = env.get('USERNAME');
+  
+  ngOnInit() {
+    console.log(this.username);
+  }
+
+  
 }
